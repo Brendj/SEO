8b59d3b4f832650790b10c467a179f143a1986df
Brendj <sergeymironov120@gmail.com>
2024-05-07 23:02:30 +0300

    фикс ошибки stompClient

--- src/main/resources/static/js/util/ws.js
+++ src/main/resources/static/js/util/ws.js
@@ -1,8 +1,10 @@
 import { Stomp } from "@stomp/stompjs"
 import SockJS from 'sockjs-client'
 
-let stompClient = Stomp.over(new SockJS('http://localhost:8081/gs-guide-websocket'))
+let stompClient = Stomp.over(() => new SockJS('http://localhost:8081/gs-guide-websocket'))
 const handlers = []
+
+stompClient.debug = function (){}
 
 stompClient.onConnect = frame => {
     console.log('Connected: ' + frame)
